c33632df3a3f8ba4c7cb7e0afa5dc8c8b0a838ad
franco66 <david_franco@outlook.com>
2023-12-02 13:34:31 -0300

    saquebgred

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -8,7 +8,7 @@ const Subcribe = () => {
         Sing Up from <span className="text-coral-red"> Update </span>
         & Newsletters
       </h3>
-      <div className="lg:max-w-[30%] min-w-[60%] flex  bg-red-500 items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+      <div className="lg:max-w-[30%] min-w-[60%] flex  items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="suscribe@nike.com"
         className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
